0164fe255f2a53153134308394fd03c101b812ff
oktaniap <61197275+oktaniap@users.noreply.github.com>
2020-03-10 21:50:57 +0700

    Delete t1x.php

--- praktikum5/Tugas 1/t1x.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "t1.php";
-echo "<center>Nama :".$_POST['nama']."</center><br>";
-echo "<center>Email :".$_POST['email']."</center><br>";
-echo "<center>Tanggal Login :".$tgl." pukul ".$wkt."</center><br>";
-?>
